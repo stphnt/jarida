9f6a1e5c6e4f89417cd267c815f41e6d3f9f5ed6
Stephen Hunt <stphnhnt.git@gmail.com>
2021-03-02 21:30:21 -0800

    Parse CLI arguments before asking for credentials

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ use journal::{get_and_validate_credentials, Args, Config, Store};
 fn main() -> anyhow::Result<()> {
     pretty_env_logger::init();
 
+    let command = Args::from_args();
     let cfg = Config::find()?;
     let mut db = Store::open(cfg.data_store_path())?;
     let (username, mut data_guard) = get_and_validate_credentials(&cfg, &mut db)?;
     let mut db = db.guard(&mut data_guard, &username);
-
-    Args::from_args().run(&cfg, &mut db)
+    command.run(&cfg, &mut db)
 }
