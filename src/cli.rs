9216013e82cabe383ab7d0eda1c8b495a035ebde
Stephen Hunt <stphnhnt.git@gmail.com>
2022-02-26 15:48:49 -0800

    Replace StructOpt with clap v3

--- src/cli.rs
+++ src/cli.rs
@@ -4,9 +4,15 @@ use super::{
 };
 use std::path::PathBuf;
 
-#[derive(Debug, structopt::StructOpt)]
-#[structopt(name = "journal", rename_all = "kebab-case")]
-pub enum Args {
+#[derive(Debug, clap::Parser)]
+#[clap(version, about, long_about = None)]
+pub struct Args {
+    #[clap(subcommand)]
+    action: Action,
+}
+
+#[derive(Debug, clap::Subcommand)]
+pub enum Action {
     /// Create a new journal entry
     New,
     /// List all existing journal entries
@@ -16,7 +22,7 @@ pub enum Args {
         /// The ID of the entry to show
         id: Option<Uuid>,
         /// Whether to print the entry in TOML format instead of the default
-        #[structopt(long, short)]
+        #[clap(long, short)]
         toml: bool,
     },
     /// Edit an existing journal entry
@@ -37,19 +43,19 @@ pub enum Args {
 
 impl Args {
     pub fn run(&self, cfg: &Config, db: &mut GuardedStore) -> anyhow::Result<()> {
-        match self {
-            Args::New => new_entry(cfg, db),
-            Args::List => print_entry_list(db),
-            Args::Show { id, toml } => {
+        match &self.action {
+            Action::New => new_entry(cfg, db),
+            Action::List => print_entry_list(db),
+            Action::Show { id, toml } => {
                 if let Some(id) = id {
                     print_entry(db, *id, if *toml { Format::Toml } else { Format::Default })
                 } else {
                     print_all_entries(db, if *toml { Format::Toml } else { Format::Default })
                 }
             }
-            Args::Edit { id } => edit_entry(cfg, db, *id),
-            Args::Index => db.index(),
-            Args::Init { dir } => init(dir.clone()),
+            Action::Edit { id } => edit_entry(cfg, db, *id),
+            Action::Index => db.index(),
+            Action::Init { dir } => init(dir.clone()),
         }
     }
 }
